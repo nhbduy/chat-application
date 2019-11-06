8164169048d08a6eef4ae827739ff72a3afe3bdf
Duy Nguyen <duy.nguyen@christinas.vn>
2019-11-06 12:02:23 +0700

    Updates

---
+++ precache-manifest.defc990d59693fbc08f54e8cfb48f35d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "483dcc745aab1de9f642c95fa48285fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e3d16b5056b18c84092",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "368106b57401096240e1",
+    "url": "/static/js/2.eb88d30f.chunk.js"
+  },
+  {
+    "revision": "7e3d16b5056b18c84092",
+    "url": "/static/js/main.da5a6ae9.chunk.js"
+  },
+  {
+    "revision": "c0f17a8994fe63ee68c5",
+    "url": "/static/js/runtime-main.6b92708b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
