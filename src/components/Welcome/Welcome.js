26be6ebf3c56a9ec12a0da75ce6c2dc1e0d378b9
Hoang-Bao-Duy NGUYEN <duy3105@gmail.com>
2019-11-15 17:23:00 +0700

    Updating Welcome screen

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
+import queryString from 'query-string';
 import { Link } from 'react-router-dom';
 
-function Welcome() {
-  const [userName, setUserName] = useState('');
-  const [roomName, setRoomName] = useState('');
+function Welcome({ location }) {
+  // check custom link (user and/or roomName)
+  const paramsURL = location.search;
+  const { user = '', room = '' } = queryString.parse(paramsURL);
+
+  const [userName, setUserName] = useState(user);
+  const [roomName, setRoomName] = useState(room);
 
   function handleOnChangeUsername(event) {
     const { value = '' } = event.target;
@@ -25,23 +30,37 @@ function Welcome() {
 
   return (
     <div className='h-100 d-flex justify-content-center flex-column p-5'>
-      <h1 className='p-3 text-center'>Welcome</h1>
+      <h1 className='text-center'>Welcome</h1>
+      <hr className='p-4 w-25 border-info' />
       <div>
-        <div className='form-group'>
+        <div className='form-group d-flex flex-column'>
+          <label
+            htmlFor='username'
+            className='p-0 m-0 font-weight-bold align-self-center'>
+            Username
+          </label>
           <input
+            value={userName}
             type='username'
             className='form-control'
             id='username'
-            placeholder='Username'
+            placeholder='Type your username...'
             onChange={handleOnChangeUsername}
           />
         </div>
-        <div className='form-group'>
+        <div className='form-group d-flex flex-column'>
+          <label
+            htmlFor='roomname'
+            className='p-0 m-0 font-weight-bold align-self-center'>
+            Room
+          </label>
           <input
+            value={roomName}
+            disabled={roomName}
             type='text'
             className='form-control'
-            id='roomName'
-            placeholder='Room name'
+            id='roomname'
+            placeholder='Type room name...'
             value={roomName}
             onChange={handleOnChangeRoomName}
           />
